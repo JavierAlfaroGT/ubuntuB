9cc40a43dcff6da604711f28454d535a8c147086
JavierAlfaroGT <javieralfarogt@gmail.com>
2022-01-31 16:18:13 -0600

    hello

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// C++ program to display "Hello World"
+//F6 compile
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+ 
+// Main() function: where the execution of program begins
+int main()
+{
+    // prints hello world
+    cout << "Hello World\nbye world xd"<<endl;
+ 
+    return 0;
+}
